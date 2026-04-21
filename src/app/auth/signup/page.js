fa36497e3fa2f7360dcf888d50bb5d5be5b842b3
ManishDeorari <manishdeorari377@gmail.com>
2026-04-21 23:52:28 +0530

    Fixed unescaped quote issue for production

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -125,7 +125,7 @@ export default function SignupPage() {
       darkMode={darkMode}
       tubeCount={10}
     >
-      <div className="min-h-screen flex flex-col lg:flex-row items-center lg:items-start lg:pt-20 justify-center lg:justify-start px-4 sm:px-8 pb-32 sm:pb-0 transition-colors duration-500 text-white">
+      <div className="min-h-screen flex flex-col lg:flex-row items-center lg:items-start lg:pt-12 justify-center lg:justify-start px-4 sm:px-8 pb-32 sm:pb-0 transition-colors duration-500 text-white">
       <LoadingOverlay isVisible={loading} message="Creating Account..." />
 
       <div className="w-full max-w-7xl mx-auto flex flex-col lg:flex-row items-center lg:items-start justify-between gap-12 z-10">
@@ -133,7 +133,7 @@ export default function SignupPage() {
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
-          className="w-full lg:w-1/2 max-w-[310px] sm:max-w-[420px] lg:max-w-[500px] lg:pl-4 mt-6 sm:mt-12 lg:mt-0 mb-8 mx-auto lg:mx-0"
+          className="w-full lg:w-1/2 max-w-[310px] sm:max-w-[420px] lg:max-w-[450px] lg:-ml-12 mt-6 sm:mt-12 lg:mt-0 mb-8 mx-auto lg:mx-0"
         >
           <div className="p-[2px] sm:p-[2.5px] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-[2rem] sm:rounded-[2.5rem] shadow-2xl relative">
             <div className={`${darkMode ? "bg-[#0f172a]/95 text-white" : "bg-[#FAFAFA] text-gray-900"} backdrop-blur-2xl rounded-[calc(2rem-2px)] sm:rounded-[calc(2.5rem-2.5px)] py-4 px-5 sm:px-8 md:py-5 md:px-10 space-y-3 relative overflow-hidden transition-all duration-500`}>
