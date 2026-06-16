7bc4e61e25923e6fc151516617ccaa9c66f2debc
ManishDeorari <manishdeorari377@gmail.com>
2026-06-16 13:46:07 +0530

    Fixed unescaped quote issue for production

--- src/app/components/profile/PointsDistributionModal.js
+++ src/app/components/profile/PointsDistributionModal.js
@@ -165,7 +165,7 @@ export default function PointsDistributionModal({ isOpen, onClose, user }) {
                                                         </div>
                                                     </motion.div>
                                                 )}
-                                            </AnimatePresence>              </AnimatePresence>
+                                            </AnimatePresence>
                                         </div>
                                     </div>
                                 ))}
